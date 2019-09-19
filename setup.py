b3226f2163f6828918137ef10157da2aec9bf6d8
François GUÉRIN <fguerin@ville-tourcoing.fr>
2019-09-19 10:03:09 +0200

    Adds packages to setup

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setuptools.setup(
         "Click",
         "PyYAML"
     ],
+    packages=setuptools.find_packages(),
     entry_points="""
      [console_scripts]
      bump_release=bump_release:bump_release
